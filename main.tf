86cdb5596f1141db0ac836f6bfd68d9f4473e99d
krishna-yadav1 <krishan.yadav@slovink.com>
2025-03-28 11:44:49 +0530

    add more features

--- main.tf
+++ main.tf
@@ -13,29 +13,52 @@ data "google_client_config" "current" {
 #####==============================================================================
 ##### Each firewall has its own firewall controlling access to and from the instances.
 #####==============================================================================
-resource "google_compute_firewall" "default" {
-  count     = var.enabled && var.firewall_enabled ? 1 : 0
-  name      = format("%s-firewall", module.labels.id)
-  network   = var.network
-  direction = var.direction
-  disabled  = var.disabled
-  priority  = var.priority
-  project   = data.google_client_config.current.project
-  dynamic "allow" {
-    for_each = var.allow
+resource "google_compute_firewall" "rules" {
+  for_each = {
+    for r in concat(var.ingress_rules, var.egress_rules) : r.name => r
+    if r.direction == "INGRESS" || r.direction == "EGRESS"
+  }
 
+  name        = each.value.name
+  description = each.value.description
+  direction   = each.value.direction # INGRESS or EGRESS
+  disabled    = each.value.disabled
+  network     = var.network
+  project     = data.google_client_config.current.project
+
+  # For ingress rules
+  source_ranges           = lookup(each.value, "source_ranges", null)
+  source_tags             = lookup(each.value, "source_tags", null)
+  source_service_accounts = lookup(each.value, "source_service_accounts", null)
+  target_tags             = lookup(each.value, "target_tags", null)
+  target_service_accounts = lookup(each.value, "target_service_accounts", null)
+
+  # For egress rules
+  destination_ranges = lookup(each.value, "destination_ranges", null)
+
+  priority = each.value.priority
+
+  dynamic "log_config" {
+    for_each = lookup(each.value, "log_config", null) == null ? [] : [each.value.log_config]
+    content {
+      metadata = log_config.value.metadata
+    }
+  }
+
+  dynamic "allow" {
+    for_each = lookup(each.value, "allow", [])
     content {
       protocol = allow.value.protocol
-      ports    = allow.value.ports
+      ports    = lookup(allow.value, "ports", null)
     }
   }
 
   dynamic "deny" {
-    for_each = var.deny
+    for_each = lookup(each.value, "deny", [])
     content {
       protocol = deny.value.protocol
-      ports    = deny.value.ports
+      ports    = lookup(deny.value, "ports", null)
+      # No source_ranges needed here for deny blocks in the firewall resource
     }
   }
-  source_ranges = var.source_ranges
 }
